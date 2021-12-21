4370141ad5fe14585915dc1e09c894aff8d176c2
leog <leog91@gmail.com>
2021-12-21 03:24:46 +0100

    upgrade react, next, tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 };
