991c3f8e8f32af23a377ab58d1692b0dca8b8c16
leog <leog91@gmail.com>
2022-01-03 03:04:20 +0100

    walker > object interaction

--- pages/walker.js
+++ pages/walker.js
@@ -1,5 +1,18 @@
 import { useState, useEffect } from "react";
 
+const RIGHT = "RIGHT";
+const LEFT = "LEFT";
+const UP = "UP";
+const DOWN = "DOWN";
+
+const MAP_ELEM = {
+  TREE: { desc: "has leafs", code: "tree", interact: "feels rough" },
+  ROCK: { desc: "heavy", code: "rock" },
+  MUSHROOM: { desc: "power up", code: "mushroom" },
+};
+
+const canPick = [MAP_ELEM.MUSHROOM];
+
 function Walker() {
   //   const grid = [
   //     ["O", "O", "O", "O"],
@@ -7,6 +20,8 @@ function Walker() {
   //     ["O", "X", "O", "O"],
   //     ["O", "O", "O", "O"],
   //   ];
+
+  //check keys, idx(ixiy)
 
   // render mode
   // center(zelda, pokemon)
@@ -24,9 +39,15 @@ function Walker() {
 
   const [map, setMap] = useState([[]]);
 
+  const [interactionTarget, setInteractionTarget] = useState(null);
+
   const initialMap = Array(10)
     .fill(Array(10).fill(""))
     .map((c, ix) => c.map((elem, ir) => `x${ix}y${ir}`));
+
+  initialMap[3][3] = "tree";
+
+  initialMap[5][3] = "mushroom";
 
   // column row
 
@@ -49,6 +70,7 @@ function Walker() {
   const [walkerPosition, setWalkerPosition] = useState({
     x: 0,
     y: 0,
+    direction: RIGHT,
   });
 
   const move = () => {
@@ -92,21 +114,29 @@ function Walker() {
 
   const moveDown = () => {
     if (walkerPosition.y === 9) return;
-    setWalkerPosition({ ...walkerPosition, y: walkerPosition.y + 1 });
+    walkerPosition.direction === DOWN
+      ? setWalkerPosition({ ...walkerPosition, y: walkerPosition.y + 1 })
+      : setWalkerPosition({ ...walkerPosition, direction: DOWN });
   };
 
   const moveLeft = () => {
     if (walkerPosition.x === 0) return;
-    setWalkerPosition({ ...walkerPosition, x: walkerPosition.x - 1 });
+    walkerPosition.direction === LEFT
+      ? setWalkerPosition({ ...walkerPosition, x: walkerPosition.x - 1 })
+      : setWalkerPosition({ ...walkerPosition, direction: LEFT });
   };
   const moveRight = () => {
     if (walkerPosition.x === 9) return;
-    setWalkerPosition({ ...walkerPosition, x: walkerPosition.x + 1 });
+    walkerPosition.direction === RIGHT
+      ? setWalkerPosition({ ...walkerPosition, x: walkerPosition.x + 1 })
+      : setWalkerPosition({ ...walkerPosition, direction: RIGHT });
   };
 
   const moveUp = () => {
     if (walkerPosition.y === 0) return;
-    setWalkerPosition({ ...walkerPosition, y: walkerPosition.y - 1 });
+    walkerPosition.direction === UP
+      ? setWalkerPosition({ ...walkerPosition, y: walkerPosition.y - 1 })
+      : setWalkerPosition({ ...walkerPosition, direction: UP });
   };
 
   const reset = () => {
@@ -119,6 +149,10 @@ function Walker() {
     ]);
     setStep(0);
   };
+
+  useEffect(() => {
+    setInteractionTarget(null);
+  }, [walkerPosition]);
 
   const handler = (e) => {
     // console.log("handler", e);
@@ -139,7 +173,40 @@ function Walker() {
         moveDown();
         break;
 
+      case "x":
+        console.log("pressing x");
+
+        console.log(walkerPosition.direction);
+        // switch (walkerPosition.direction) {
+        if (
+          walkerPosition.direction === DOWN &&
+          map[walkerPosition.x][walkerPosition.y + 1] === "tree"
+        )
+          setInteractionTarget(MAP_ELEM.TREE);
+
+        if (
+          walkerPosition.direction === UP &&
+          map[walkerPosition.x][walkerPosition.y - 1] === "tree"
+        )
+          setInteractionTarget(MAP_ELEM.TREE);
+        if (
+          walkerPosition.direction === RIGHT &&
+          map[walkerPosition.x + 1][walkerPosition.y] === "tree"
+        )
+          setInteractionTarget(MAP_ELEM.TREE);
+        if (
+          walkerPosition.direction === LEFT &&
+          map[walkerPosition.x - 1][walkerPosition.y] === "tree"
+        )
+          setInteractionTarget(MAP_ELEM.TREE);
+
+        break;
+
+      case "z":
+        setInteractionTarget(null);
+        break;
       default:
+        console.log("key", e.key);
         break;
     }
   };
@@ -170,33 +237,51 @@ function Walker() {
             ))}
           </div>
 
-          <div className="grid grid-cols-3 text-center text-2xl ">
-            <div> </div>
+          <div className="flex m-4">
+            <div className="grid mx-14 grid-cols-3 text-center text-2xl ">
+              <div> </div>
 
-            <div
-              onClick={() => moveUp()}
-              className="flex rounded-md justify-center items-center bg-slate-400 w-14 h-14 m-1 hover:bg-slate-200 hover:text-black"
-            >
-              ▲
+              <div
+                onClick={() => moveUp()}
+                className="flex rounded-md justify-center items-center bg-slate-400 w-14 h-14 m-1 hover:bg-slate-200 hover:text-black"
+              >
+                ▲
+              </div>
+              <div> </div>
+              <div
+                onClick={() => moveLeft()}
+                className="flex rounded-md justify-center items-center bg-slate-400 w-14 h-14 m-1 hover:bg-slate-200 hover:text-black"
+              >
+                ◄
+              </div>
+              <div
+                onClick={() => moveDown()}
+                className="flex rounded-md justify-center items-center bg-slate-400 w-14 h-14 m-1 hover:bg-slate-200 hover:text-black"
+              >
+                ▼
+              </div>
+              <div
+                onClick={() => moveRight()}
+                className="flex rounded-md justify-center items-center bg-slate-400 w-14 h-14 m-1 hover:bg-slate-200 hover:text-black"
+              >
+                ►
+              </div>
             </div>
-            <div> </div>
-            <div
-              onClick={() => moveLeft()}
-              className="flex rounded-md justify-center items-center bg-slate-400 w-14 h-14 m-1 hover:bg-slate-200 hover:text-black"
-            >
-              ◄
-            </div>
-            <div
-              onClick={() => moveDown()}
-              className="flex rounded-md justify-center items-center bg-slate-400 w-14 h-14 m-1 hover:bg-slate-200 hover:text-black"
-            >
-              ▼
-            </div>
-            <div
-              onClick={() => moveRight()}
-              className="flex rounded-md justify-center items-center bg-slate-400 w-14 h-14 m-1 hover:bg-slate-200 hover:text-black"
-            >
-              ►
+
+            <div className="flex bg-blue-500 items-center   rounded-3xl">
+              TODO
+              <div className="flex flex-col items-center">
+                <div>press z</div>
+                <button className="rounded-full m-2 w-16 h-16 bg-blue-900 font-black text-lg">
+                  z
+                </button>
+              </div>
+              <div className="flex flex-col items-center">
+                <div>press z</div>
+                <button className="rounded-full m-2 w-16 h-16 bg-blue-900 font-black text-lg">
+                  x
+                </button>
+              </div>
             </div>
           </div>
 
@@ -204,25 +289,66 @@ function Walker() {
             {" "}
             currentPos = x:{walkerPosition.x} / y:{walkerPosition.y}
           </div>
+          <div>direction = {walkerPosition.direction}</div>
 
-          <div className="flex p-3 m-3 bg-blue-900">
-            {map.map((e, i) => (
-              <div key={i} className=" text-center ">
-                {/* [x:{i}]{" "} */}
-                {e.map((j) => (
-                  <div
-                    className="m-1 bg-blue-500 h-8 w-8 hover:bg-red-200"
-                    key={j}
-                  >
-                    {j === "player" ? "x" : ""}
-                    {/* {}
+          <div className="flex">
+            <div className="flex p-3 m-3 bg-blue-900">
+              {map.map((e, i) => (
+                <div key={i} className=" text-center ">
+                  {/* [x:{i}]{" "} */}
+                  {e.map((j) => (
+                    <div
+                      className="m-1 bg-blue-500 h-8 w-8 hover:bg-red-200"
+                      key={j}
+                    >
+                      {/* ➤{j === "player" ? "x" : ""} */}
+                      {j === "player"
+                        ? walkerPosition.direction === RIGHT
+                          ? "►"
+                          : walkerPosition.direction === LEFT
+                          ? "◄"
+                          : walkerPosition.direction === UP
+                          ? "▲"
+                          : "▼ "
+                        : // : ""}
+                        j === "tree"
+                        ? "🌳"
+                        : j === "mushroom"
+                        ? "🍄"
+                        : ""}
+
+                      {/* {}
                     {j}{" "} */}
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
 
+            <div className="bg-sky-800 w-56 h-36 m-3 p-3">
+              <div className="font-bold text-center">*interaction box*</div>
+              <div>
+                {interactionTarget && (
+                  <div>
+                    {" "}
+                    <div>
+                      {">"} {interactionTarget.code}
+                    </div>
+                    {interactionTarget.desc && (
+                      <div>
+                        {">"} {interactionTarget.desc}{" "}
+                      </div>
+                    )}
+                    {interactionTarget.interact && (
+                      <div>
+                        {"> action - "} {interactionTarget.interact}
+                      </div>
+                    )}
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
           <button
             onClick={() => move()}
             onKeyPress={(e) => handler(e)}
