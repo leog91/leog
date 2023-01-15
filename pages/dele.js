432eb0c9ddaf7ef1a70c50c99309d3baa4a07eff
leog <leog91@gmail.com>
2023-01-15 03:53:17 +0100

    animation test

---
+++ pages/dele.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Dele() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-indigo-400">
+      <div className="rounded-sm bg-indigo-800 p-2 text-indigo-100">qwe</div>
+      <div className="road" style={{}}>
+        {" "}
+        qwe
+      </div>
+    </div>
+  );
+}
+
+export default Dele;
