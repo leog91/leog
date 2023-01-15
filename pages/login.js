85bee5af8a7abc5cb3c8cc18182ead629aace8a6
leog <leog91@gmail.com>
2023-01-15 03:55:50 +0100

    login page

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LogIn from "../components/login";
+
+// const "bg-purple-600"
+
+function Login() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center  bg-neutral-200 text-white ">
+      <LogIn />
+    </div>
+  );
+}
+
+export default Login;
