3912163dd77477895291b95e3c74cb7c532d51cd
leog <leog91@gmail.com>
2023-01-15 03:54:51 +0100

    login component redisign

--- components/login.js
+++ components/login.js
@@ -1,28 +1,38 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 export default function LogIn() {
   const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button
-          className="m-2 rounded-md bg-slate-700 p-2"
-          onClick={() => signOut()}
-        >
-          Sign out
-        </button>
-      </>
-    );
-  }
+
   return (
-    <>
-      Not signed in <br />
+    <div className="flex w-full  max-w-md flex-col items-center rounded-sm bg-neutral-800 px-4 py-4">
+      <h3 className="mt-4 mb-9 text-lg font-semibold">
+        {session ? (
+          <div className="flex">
+            Signed in as{" "}
+            <p className=" px-2 text-purple-500/90 underline">
+              {" "}
+              {session.user.email}
+            </p>
+          </div>
+        ) : (
+          "Not signed in"
+        )}
+      </h3>
+
       <button
-        className=" m-2  rounded-md bg-slate-700 p-2"
-        onClick={() => signIn()}
+        className="5 my-1 w-full rounded-sm bg-purple-500/90 text-sm font-bold  hover:bg-purple-500/70"
+        onClick={() => (session ? signOut() : signIn())}
       >
-        Sign in
+        {session ? "Sign out" : "Log in"}
       </button>
-    </>
+
+      {!session && (
+        <p
+          onClick={() => alert("good luck!")}
+          className="cursor-pointer text-xs text-purple-500/90 hover:underline"
+        >
+          Trouble logging in?
+        </p>
+      )}
+    </div>
   );
 }
